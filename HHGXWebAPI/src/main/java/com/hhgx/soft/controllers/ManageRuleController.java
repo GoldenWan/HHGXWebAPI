071b02cbf28b5336b232e24cee3571382908fc94
win <win@DESKTOP-FS3SGU9>
2017-08-17 12:58:17 +0800

    aaaaaaaa

---
+++ HHGXWebAPI/src/main/java/com/hhgx/soft/controllers/ManageRuleController.java
@@ -0,0 +1,11 @@
+package com.hhgx.soft.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="ManageRule")
+public class ManageRuleController {
+	
+
+}
---
+++ HHGXWebAPI/src/main/java/com/hhgx/soft/entitys/SafeManageRules.java
@@ -0,0 +1,59 @@
+package com.hhgx.soft.entitys;
+
+public class SafeManageRules {
+	private String safeManageRulesID;
+	private String safeManageRulesName;
+	private String uploadTime;
+	private String safeManageRulesType;
+	private String filepath;
+	private String orgid;
+
+	public String getSafeManageRulesID() {
+		return safeManageRulesID;
+	}
+
+	public void setSafeManageRulesID(String safeManageRulesID) {
+		this.safeManageRulesID = safeManageRulesID;
+	}
+
+	public String getSafeManageRulesName() {
+		return safeManageRulesName;
+	}
+
+	public void setSafeManageRulesName(String safeManageRulesName) {
+		this.safeManageRulesName = safeManageRulesName;
+	}
+
+	public String getUploadTime() {
+		return uploadTime;
+	}
+
+	public void setUploadTime(String uploadTime) {
+		this.uploadTime = uploadTime;
+	}
+
+	public String getSafeManageRulesType() {
+		return safeManageRulesType;
+	}
+
+	public void setSafeManageRulesType(String safeManageRulesType) {
+		this.safeManageRulesType = safeManageRulesType;
+	}
+
+	public String getFilepath() {
+		return filepath;
+	}
+
+	public void setFilepath(String filepath) {
+		this.filepath = filepath;
+	}
+
+	public String getOrgid() {
+		return orgid;
+	}
+
+	public void setOrgid(String orgid) {
+		this.orgid = orgid;
+	}
+
+}
