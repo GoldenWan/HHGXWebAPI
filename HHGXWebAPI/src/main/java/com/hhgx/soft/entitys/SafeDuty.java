3df574f18fac2c4f9b062453266dccd8ee5aac08
win <win@DESKTOP-FS3SGU9>
2017-08-18 09:06:13 +0800

    111

---
+++ HHGXWebAPI/src/main/java/com/hhgx/soft/entitys/SafeDuty.java
@@ -0,0 +1,62 @@
+package com.hhgx.soft.entitys;
+
+public class SafeDuty {
+/**
+ * 
+ */
+	private String safeDutyID;
+	private String dutyname;
+	private String uploadtime;
+	private String safedutytype;
+	private String filepath;
+	private String orgid;
+
+	public String getSafeDutyID() {
+		return safeDutyID;
+	}
+
+	public void setSafeDutyID(String safeDutyID) {
+		this.safeDutyID = safeDutyID;
+	}
+
+	public String getDutyname() {
+		return dutyname;
+	}
+
+	public void setDutyname(String dutyname) {
+		this.dutyname = dutyname;
+	}
+
+	public String getUploadtime() {
+		return uploadtime;
+	}
+
+	public void setUploadtime(String uploadtime) {
+		this.uploadtime = uploadtime;
+	}
+
+	public String getSafedutytype() {
+		return safedutytype;
+	}
+
+	public void setSafedutytype(String safedutytype) {
+		this.safedutytype = safedutytype;
+	}
+
+	public String getFilepath() {
+		return filepath;
+	}
+
+	public void setFilepath(String filepath) {
+		this.filepath = filepath;
+	}
+
+	public String getOrgid() {
+		return orgid;
+	}
+
+	public void setOrgid(String orgid) {
+		this.orgid = orgid;
+	}
+
+}
